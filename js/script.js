bae46dbebd53a846d1009793e96ae54ff101ce8e
Rimy Akter <rimy@gmail.com>
2025-02-13 09:36:24 +0600

    building a ecomerce website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          'primary-green': '#179800',
+          'secondary-white': '#EFEBE3',
+        }
+      }
+    }
+  }
